0b2b8449c91266f5922dd2ae88188128c56a0fcb
Joshyjosh <joshsarnecki@gmail.com>
2022-06-15 11:51:19 -0700

    Passing first few tests via looping through the acquired split rna proteins, against the mapping array key

--- protein-translation.js
+++ protein-translation.js
@@ -1,8 +1,35 @@
 
 export const translate = (rna) => {
+
+  const mapping = [
+    ['Methionine', ['AUG']],
+    ['Phenylalanine', ['UUU', 'UUC']],
+    ['Leucine', ['UUA', 'UUG']],
+    ['Serine', ['UCU', 'UCC', 'UCA', 'UCG']],
+    ['Tyrosine', ['UAU', 'UAC']],
+    ['Cysteine', ['UGU', 'UGC']],
+    ['Tryptophan', ['UGG']],
+    ['STOP', ['UAA', 'UAG', 'UGA']]
+  ];
+
   if (!rna) {
     return [];
   }
-  let split = rna.match(/.{1,3}/g);
-  console.log(split);
+
+  const split = rna.match(/.{1,3}/g);
+  const result = [];
+
+  // Loop thru rnaSplitArr, compare against mapping
+  for (let rna of split) {
+    for (let pro of mapping) {
+      if (pro[1].includes(rna)) {
+        //Push mapping[x][0] into result
+        result.push(pro[0]);
+      }
+    }
+  }
+
+  console.log(result);
+  return result;
+
 };
