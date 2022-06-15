a4697f46a22c35508a1683c2014a545e950eaace
Joshyjosh <joshsarnecki@gmail.com>
2022-06-15 12:41:02 -0700

    Refactored and added comments

--- protein-translation.js
+++ protein-translation.js
@@ -1,10 +1,6 @@
 
 export const translate = (rna) => {
   
-  if (!rna) {
-    return [];
-  }
-
   const mapping = [
     ['Methionine', ['AUG']],
     ['Phenylalanine', ['UUU', 'UUC']],
@@ -15,25 +11,33 @@ export const translate = (rna) => {
     ['Tryptophan', ['UGG']],
     ['STOP', ['UAA', 'UAG', 'UGA']]
   ];
-
+  
+  if (!rna) {
+    return [];
+  }
+  
   const split = rna.match(/.{1,3}/g);
+  // Splits the given RNA into 3-char length elms
   const result = [];
 
-  // Loop thru rnaSplitArr, compare against mapping
+  // Loop thru splitArr, compare against mapping
   for (const rna of split) {
     for (const protein of mapping) {
       if (protein[1].includes(rna)) {
-        //Push mapping[x][0] into result
+        // Check each rna elm from splitArr against the arrays of each protein
+        // Push the protein name into result when it matches
         result.push(protein[0]);
       }
     }
   }
 
   if ((!result.length && split) || (split[split.length - 1].length < 3 && !result.includes("STOP"))) {
+    // 1st: If result is empty, but split has elements, means the codon elms do not exist
+    // 2nd: If final elm in split is less than 3, it's incomplete. But if result includes STOP, it cancels out the incomplete codon
     throw new Error('Invalid codon');
   }
   
-  // Remove STOP proteins
+  // If the result contains STOP, remove it with every protein after
   if (result.includes("STOP")) {
     const stop = result.indexOf("STOP");
     result.splice(stop);
